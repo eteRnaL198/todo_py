eab850d3f017dd06618dd70e2dad8118c14459a5
skisnosuke <yakyuman.1108@icloud.com>
2021-09-02 15:24:47 +0900

    9/2 追加

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ class App:
         self.master.title('TODOアプリ')
         self.master.geometry('400x300')
 
-        self.tasks = ['foo', 'bar', 'hoge']
-        self.input_area = InputArea(self.master, self.tasks, self._reRenderTaskList)
+        # tasks → [{btn:tk.CheckButton, text:string val:boolean}, {}]
+        self.tasks = []
+        self.input_area = InputArea(self.master, self.tasks, self._remove_task)
         self.input_area.pack()
-        self.task_list = TaskList(self.master, self.tasks)
-        self.task_list.pack()
 
-    def _reRenderTaskList(self):
-        self.task_list.destroy()
-        self.task_list = TaskList(self.master, self.tasks)
-        self.task_list.pack()
+    def _remove_task(self):
+        i=0
+        for task in self.tasks:
+            if task['val'].get():
+                task['btn'].destroy()
+                self.tasks.pop(i)
+            else:
+                i += 1
+
 
     def mainloop(self):
         self.master.mainloop()
@@ -26,11 +30,11 @@ class InputArea(tk.Frame):
     ユーザーの入力の処理
     TODOテキストを追加ボタンで追加
     """
-    def __init__(self, master, tasks, reRender):
+    def __init__(self, master, tasks, remove):
         super(InputArea, self).__init__(master)
 
         self.tasks = tasks
-        self.reRender = reRender
+        self.remove = remove
 
         self.label = tk.Label(self, text='TODO')
         self.label.pack(side='left')
@@ -39,20 +43,25 @@ class InputArea(tk.Frame):
         self.entry.pack(side='left')
 
         self.add_btn = tk.Button(self, text='追加', command=self._update_task)
-        self.add_btn.pack(side='left')
+        self.add_btn.pack(anchor=tk.N)
+
+        self.rem_btn = tk.Button(self, text='削除', command=self.remove)
+        self.rem_btn.pack(anchor=tk.N)
 
     def _update_task(self):
-        self.tasks.append(self.entry.get())
-        self.reRender()
-        self.entry.delete(0, tk.END)
+        if self.entry.get():
 
-class TaskList(tk.Frame):
-    def __init__(self, master, tasks):
-        super(TaskList, self).__init__(master)
-        for task in tasks:
-            self.check_btn = tk.Checkbutton(self, text=task)
-            self.check_btn.pack()
+            self.is_checked = tk.BooleanVar()
+            self.text = tk.StringVar()
+            self.text.set(self.entry.get())
+            self.check_btn = tk.Checkbutton(self, textvariable=self.text, variable=self.is_checked)
+            self.elem = {"btn": self.check_btn, "val": self.is_checked}
+            self.tasks.append(self.elem)
+            self.tasks[len(self.tasks)-1]["btn"].pack(side='left')
 
+            self.entry.delete(0, tk.END)
+
+            
 def main():
     app = App()
     app.mainloop()
